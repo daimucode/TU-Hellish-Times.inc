b8439b721c2285ab13cb8408e814bbe329c696b4
daimucode <yoruzar@abv.bg>
2017-05-30 13:56:46 +0300

    Add files via upload

--- kursova2.c
+++ kursova2.c
@@ -4,7 +4,7 @@
 
 typedef struct Dictionary{
 
-	char transWordEng[20];
+	char transWordEng[21];
 	char transWordBg[20];
 	char transWordGer[20];
 	char typeOfWord[20];
@@ -40,62 +40,73 @@ Dictionary* readFile(char filename[]){
 void addWord(){
 	
 	int lang;
-	printf("Lang:\n");
+	printf("Choose a language:\n1.English\n2.Bulgarian\n3.German\n");
 	scanf("%d",&lang);
+	getchar();
 	
 	FILE *fp;
 	Dictionary *newWord=(Dictionary*)malloc(sizeof(Dictionary));
 
-	printf("If there is no translation, insert -1\n");
+	printf("If there is no translation in one of the other languages, hit enter.\n");
 	
 	if(lang==1){
 		fp = fopen("eng.lang","ab");
 
 		printf("\n\tEnter english word: ");
-		scanf("%s", newWord->transWordEng);
+		fgets(newWord->transWordEng,20,stdin);
+		
 
 		printf("\n\tNow enter bulgarian translation: ");
-		scanf("%s", newWord->transWordBg);
+		fgets(newWord->transWordBg,20,stdin);
+		
 
 		printf("\n\tNow enter german translation: ");
-		scanf("%s", newWord->transWordGer);
+		fgets(newWord->transWordGer,20,stdin);
+		
 	}
 	else if(lang==2){
 		fp = fopen("bg.lang","ab");
 
 		printf("\n\tEnter bulgarian word: ");
-		scanf("%s", newWord->transWordBg);
+		fgets(newWord->transWordBg,20,stdin);
+		
 
 		printf("\n\tNow enter english translation: ");
-		scanf("%s", newWord->transWordEng);
+		fgets(newWord->transWordEng,20,stdin);
+		
 
 		printf("\n\tNow enter german translation: ");
-		scanf("%s", newWord->transWordGer);
+		fgets(newWord->transWordGer,20,stdin);
+		
 	}
 	else if(lang==3){
 		fp = fopen("ger.lang","ab");
 
 		printf("\n\tEnter german word: ");
-		scanf("%s", newWord->transWordGer);
+		fgets(newWord->transWordGer,20,stdin);
+		
 
 		printf("\n\tNow enter english translation: ");
-		scanf("%s", newWord->transWordEng);
+		fgets(newWord->transWordEng,20,stdin);
+		
 
 		printf("\n\tNow enter bulgarian translation: ");
-		scanf("%s", newWord->transWordBg);	
+		fgets(newWord->transWordBg,20,stdin);
+			
 	}
 
 	printf("\n\tEnter the class of the word(noun, verb, adjective): ");
-	scanf("%s", newWord->typeOfWord);
+	fgets(newWord->typeOfWord,20,stdin);
 
-	printf("\n\tEnter an example translation: ");
-	scanf("%s", newWord->exampLine);
+	printf("\n\tEnter an example sentence: ");
+	fgets(newWord->exampLine,50,stdin);
 
-	printf("\n\tEnter the date of adding(DD/MM/YY): ");
-	scanf("%s", newWord->date);
+	printf("\n\tEnter the date of adding (DD/MM/YY): ");
+	fgets(newWord->date,21,stdin);
 
-	printf("\n\tEnter a rating of the translation(0 to 10): ");
-	scanf("%f", newWord->rating);
+	printf("\n\tEnter a rating of the translation (0 to 10): ");
+	scanf("%f", &newWord->rating);
+	
 	
 	fwrite(newWord,sizeof(Dictionary),1,fp);
 	free(newWord);
@@ -104,12 +115,14 @@ void addWord(){
 
 void search(){
 
-	char word[20];
+	char word[21];
 	int lang;
 
 	printf("Insert a word:\n");
-	scanf("%s", word);
-	printf("Choose a language to be searched.\n1.English\n2.Bulgarian\n3.German;");
+	getchar();
+	fgets(word, 20, stdin);
+
+	printf("Choose a language to be searched:\n1.English\n2.Bulgarian\n3.German\n");
 	scanf("%d", &lang);
 
 	Dictionary *dict=(Dictionary*)malloc(sizeof(Dictionary));
@@ -130,21 +143,22 @@ void search(){
 	while(dict!=NULL){
 		
 	if(lang==1){
+		printf("\n%d\n", strcmp(word, dict->transWordEng));
 		if(strcmp(dict->transWordEng, word)==0){
-			printf("In english: %s\nIn bulgarian: %s\nIn german: %s\nClass of word: %s\nExample: %s\nDate added: %f\nRating: %f\n", 
+			printf("In english: %s\nIn bulgarian: %s\nIn german: %s\nClass of word: %s\nExample: %s\nDate added: %s\nRating: %f\n", 
 				dict->transWordEng, dict->transWordBg, dict->transWordGer, dict->typeOfWord, dict->exampLine, dict->date, dict->rating);
 		}
 		
 	}
 	else if(lang==2){
 		if(strcmp(dict->transWordBg, word)==0){
-			printf("In bulgarian: %s\nIn english: %s\nIn german: %s\nClass of word: %s\nExample: %s\nDate added: %f\nRating: %f\n", 
+			printf("In bulgarian: %s\nIn english: %s\nIn german: %s\nClass of word: %s\nExample: %s\nDate added: %s\nRating: %f\n", 
 				 dict->transWordBg, dict->transWordEng, dict->transWordGer, dict->typeOfWord, dict->exampLine, dict->date, dict->rating);
 		}
 	}
 	else if(lang==3){
 		if(strcmp(dict->transWordGer, word)==0){
-			printf("In german: %s\nIn english: %s\nIn bulgarian: %s\nClass of word: %s\nExample: %s\nDate added: %f\nRating: %f\n", 
+			printf("In german: %s\nIn english: %s\nIn bulgarian: %s\nClass of word: %s\nExample: %s\nDate added: %s\nRating: %f\n", 
 				dict->transWordGer, dict->transWordEng, dict->transWordBg,  dict->typeOfWord, dict->exampLine, dict->date, dict->rating);
 		}
 	}
@@ -154,6 +168,7 @@ void search(){
 }
 
 void unique(){
+
 	Dictionary *dict=(Dictionary*)malloc(sizeof(Dictionary));
 	Dictionary *dict2=(Dictionary*)malloc(sizeof(Dictionary));
 	Dictionary *dict3=(Dictionary*)malloc(sizeof(Dictionary));
@@ -171,6 +186,7 @@ void unique(){
 	for (dict = head1; dict != NULL; dict=dict->next) {
 		for (dict2 = head2; dict2!=NULL; dict2=dict2->next) {
 			if(strcmp(dict->transWordEng, dict2->transWordBg) == 0)	
+
 				isWordFound = 1;		
 		}
 
@@ -180,13 +196,14 @@ void unique(){
 		}
 		
 		if(!isWordFound)
-			printf("%s\n", dict->transWordEng);
+			printf("%s\n%s\n%s\n", &dict->transWordEng, &dict2->transWordBg, &dict3->transWordGer);
 
 		isWordFound = 0;
 	}
+
 }
 
-int main(int argc, char** argv) {
+int main() {
 
 	int menu;
 
@@ -216,5 +233,3 @@ int main(int argc, char** argv) {
 
 	return 0;
 }
-
-
